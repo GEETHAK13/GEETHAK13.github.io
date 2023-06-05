437b97815903c2f8ccb4600f7bc1e6812a23a33d
GEETHAK13 <107195059+GEETHAK13@users.noreply.github.com>
2023-06-05 22:31:02 +0530

    Day7 Update

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Being zero")
+var data1 = {
+    image: "./girl.png",
+    name: "Geetha",
+    designation: "Software Developer",
+    };
+var data2 = {
+    image: "./girl2.jpeg",
+    name: "Geetha Koritala",
+    designation: "Intern",
+};
+
+var isdata1 = true;
+var displaydata;
+var changepic = function () {
+    if (isdata1) {
+        displaydata = data2;
+        isdata1 = false;
+    } else {
+        displaydata = data1;
+        isdata1 = true;
+    }
+    document.getElementById("myimage").src = displaydata.image;
+    document.getElementById("myname").innerHTML = displaydata.name;
+    document.getElementById("mydesig").innerHTML = displaydata.designation;
+};
+  
